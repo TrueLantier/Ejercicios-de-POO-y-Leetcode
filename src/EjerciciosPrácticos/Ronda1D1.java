f7088b6c3fe367ffa564bcbee3f0ad181a80a0bc
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-01 10:23:45 -0500

    Segundo ejercicio hecho.

--- src/EjerciciosPrácticos/Ronda1D1.java
+++ src/EjerciciosPrácticos/Ronda1D1.java
@@ -8,6 +8,36 @@ public class Ronda1D1 {
 }
 
 /*
+Ej2: Crea una clase Calculadora con métodos para sumar, restar, multiplicar y dividir dos números (double). Incluye
+un método mostrarOperaciones() que liste todas las operaciones posibles.
+En el main:
+        Calculadora c1 = new Calculadora();
+        System.out.println(c1.suma(1,2) );
+        System.out.println(c1.resta(10,222));
+        System.out.println(c1.multiplicación(99, 888));
+        System.out.println(c1.división(1,0));
+        System.out.println(c1.división(22,7));
+        c1.mostrarInfo();
+ */
+class Calculadora {
+    double suma( double a, double b) { return a + b; }
+    double resta(double a, double b) { return a - b; }
+    double multiplicación(double a, double b) { return a * b; }
+    double división(double a, double b) {
+        if (b == 0) {
+            System.out.println("Operación inválida. División por cero.");
+            return 0.0;
+        }
+        return a / b;
+    }
+
+    void mostrarInfo() {
+        System.out.println("Operaciones:");
+        System.out.println("Sumar \nRestar \nMultiplicar \nDividir");
+    }
+}
+
+/*
 Ej1: Crea una clase persona con atributos: nombre(String), edad (int). Agrega un método mostrarInfo() que
 imprima los datos. En el main, crea 2 objetos y muestra su información.
 En el main:
@@ -28,11 +58,4 @@ class Persona {
         System.out.println(edad);
         System.out.println();
     }
-}
-
-/*
-Ej2: Crea una clase Calculadora con métodos para sumar, restar, multiplicar y dividir dos números (double). Incluye
-un método mostrarOperaciones() que liste todas las operaciones posible.
- */
-class Calculadora {
 }
